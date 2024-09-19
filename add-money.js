cad784204df4bb4d137a8c90cda1943ef6384bd1
Abdallah-BinSaif <binsaif.info@gmail.com>
2024-09-20 01:57:57 +0600

    cash out

--- add-money.js
+++ add-money.js
@@ -1,20 +1,20 @@
-document.getElementById("btn-add-money")
-.addEventListener("click",function(event){
-    event.preventDefault();
+// document.getElementById("btn-add-money")
+// .addEventListener("click",function(event){
+//     event.preventDefault();
 
 
-    const addAmount = document.getElementById("add-money-amount");
+//     const addAmount = document.getElementById("add-money-amount");
 
-    const pinNum = document.getElementById("add-money-pin-num");
+//     const pinNum = document.getElementById("add-money-pin-num");
 
 
-    if (pinNum.value === "5") {
-        const currentBalance = document.getElementById("current-balance");
+//     if (pinNum.value === "5") {
+//         const currentBalance = document.getElementById("current-balance");
 
-        let newBalance = parseFloat(currentBalance.innerText) + parseInt(addAmount.value);
+//         let newBalance = parseFloat(currentBalance.innerText) + parseInt(addAmount.value);
 
-        currentBalance.innerText = newBalance;
-        addAmount.value = "";
-        pinNum.value = "";
-    }
-})
+//         currentBalance.innerText = newBalance;
+//         addAmount.value = "";
+//         pinNum.value = "";
+//     }
+// })
--- home.js
+++ home.js
@@ -1,4 +1,28 @@
+// log out button
+
 document.getElementById("btn-logout")
 .addEventListener("click", function(){
     window.location.href = "./index.html"
+})
+
+// add money
+document.getElementById("btn-add-money")
+.addEventListener("click",function(event){
+    event.preventDefault();
+
+
+    const addAmount = document.getElementById("add-money-amount");
+
+    const pinNum = document.getElementById("add-money-pin-num");
+
+
+    if (pinNum.value === "5") {
+        const currentBalance = document.getElementById("current-balance");
+
+        let newBalance = parseFloat(currentBalance.innerText) + parseInt(addAmount.value);
+
+        currentBalance.innerText = newBalance;
+        addAmount.value = "";
+        pinNum.value = "";
+    }
 })
